2240abb8083a1dd6787625470c541188cf5a4b3f
IanFlickinger <47579368+IanFlickinger@users.noreply.github.com>
2019-11-05 15:40:04 -0600

    Alphabetical sort
    The filtering methods now return the collections separated by genre and or rating. An alphabetical sort method has been added

--- MovieFinalProject/src/Movie.java
+++ MovieFinalProject/src/Movie.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -43,7 +46,30 @@ public class Movie {
 	public void setShowTimes(String[] showTimes) {
 		this.showTimes = showTimes;
 	}
-
+	
+	
+	
+	
+	/*Sort collection of movies alphabetically
+	 * Parameters:
+	 * 	Collection<Movie> movies	- a collection of movies to be sorted
+	 * 	
+	 * Returns: a collection of the movies in alphabetical order
+	 */
+	public static Collection<Movie> sortByTitle(Collection<Movie> movies) {
+		Collection<Movie> movieList = new ArrayList<Movie>(movies);
+		
+		//Use Collections.sort method to sort the list of movies by title
+		//Instantiate a comparator to pass the method of comparison
+		Collections.sort((ArrayList<Movie>) movieList, new Comparator<Movie>() {
+			@Override
+			public int compare(Movie o1, Movie o2) {
+				return o1.getTitle().compareTo(o2.getTitle());
+			}
+		});
+		
+		return movieList;
+	}
 	
 	/*Create a collection of movies with only the given genres
 	 * Parameters:
@@ -51,19 +77,23 @@ public class Movie {
 	 * 	String... genres			- variable arity of genres to include
 	 * 
 	 * Returns a collection of movies. This collection contains every movie from the 
-	 * 	movies parameter with a given genre
+	 * 	movies parameter with a given genre. The movies are separated by genre.
 	 * 
 	 * NOTE: the genres can be chosen in the gui using check boxes
 	 */
 	public static Collection<Movie> filterGenres(Collection<Movie> movies, String...genres) {
-		Iterator<Movie> list = movies.iterator();
+		Iterator<Movie> list;
 		Collection<Movie> selectedMovies = new LinkedList<Movie>();
-		while (list.hasNext()) {
-			Movie movie = list.next();
-			for (String acceptableGenre : genres)
+		
+		for (String acceptableGenre : genres) {
+			list = movies.iterator();
+			while (list.hasNext()) {
+				Movie movie = list.next();
 				if (movie.genre.equals(acceptableGenre))
 					selectedMovies.add(movie);
+			}
 		}
+		
 		return selectedMovies;
 	}
 	
@@ -73,19 +103,24 @@ public class Movie {
 	 * 	String... ratings			- variable arity of ratings to include
 	 * 
 	 * Returns a collection of movies. This collection contains every movie from the 
-	 * 	movies parameter with a rating given in the ratings parameter
+	 * 	movies parameter with a rating given in the ratings parameter. The movies are
+	 * 	separated by rating
 	 * 
 	 * NOTE: the ratings can be chosen from the gui using check boxes
 	 */
 	public static Collection<Movie> filterRatings(Collection<Movie> movies, String...ratings) {
-		Iterator<Movie> list = movies.iterator();
+		Iterator<Movie> list;
 		Collection<Movie> selectedMovies = new LinkedList<Movie>();
-		while (list.hasNext()) {
-			Movie movie = list.next();
-			for (String acceptableRating : ratings)
+		
+		for (String acceptableRating : ratings) {
+			list = movies.iterator();
+			while (list.hasNext()) {
+				Movie movie = list.next();
 				if (movie.mpaaRating.equals(acceptableRating))
 					selectedMovies.add(movie);
+			}
 		}
+		
 		return selectedMovies;	
 	}
 }
