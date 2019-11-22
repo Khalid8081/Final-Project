a92bb5b7ae4e2f88c45220f3508ab44344bb9e14
Khalid <dw2206rq@go.minnstate.edu>
2019-11-21 19:07:55 -0800

    Added:
    Changes to the main GUI
    Sort button
    Jcombobox with sorted movies
    SortListener

--- MovieFinalProject/src/TheaterAppGUI.java
+++ MovieFinalProject/src/TheaterAppGUI.java
@@ -10,7 +10,11 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import javax.swing.BorderFactory;
@@ -24,11 +28,19 @@ import javax.swing.JTextField;
 public class TheaterAppGUI {
 
 	private JFrame frame;
+	public String sortOption;
+	public JComboBox comboBox;
+	public JComboBox comboBox_1;
 	private static final int WIDTH = 1600;
 	private static final int HEIGHT = 1050;
 	private static final String CARD1 = "A-Z";
-	private static final String CARD2 = "Genre";
-	private static final String CARD3 = "MPAA";
+	private static final String CARD2 = "Drama";
+	private static final String CARD3 = "Thriller";
+	private static final String CARD4 = "Adventure";
+	private static final String CARD5 = "Fantasy";
+	private static final String CARD6 = "R";
+	private static final String CARD7 = "PG";
+	private static final String CARD8 = "PG-13";
 	
 	private JTextField searchTextField;
 	
@@ -60,11 +72,12 @@ public class TheaterAppGUI {
 		movieCollection.add(movie2);
 		movieCollection.add(movie3);
 		movieCollection.add(movie4);
-		movieCollection.add(movie4);
+		movieCollection.add(movie5);
 		initialize(); 
+		
 	}
 		
-	private void initialize()  {
+	public void initialize()  {
 		frame = new JFrame();
 		frame.setTitle("Movie Theater");
 		frame.setSize(WIDTH, HEIGHT);
@@ -73,7 +86,7 @@ public class TheaterAppGUI {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
@@ -119,7 +132,7 @@ public class TheaterAppGUI {
 		enterButton.addActionListener(new SearchListener()); 	//SearchListener() not implemented
 		frame.getContentPane().add(enterButton, gbc_enterButton);
 		
-		JLabel nowShowingLabel = new JLabel("Now Showing:");
+		JLabel nowShowingLabel = new JLabel("Sort by:");
 		nowShowingLabel.setBackground(Color.WHITE);
 		nowShowingLabel.setFont(new Font("HelveticaNeue", Font.BOLD, 18));
 		nowShowingLabel.setForeground(Color.BLACK);
@@ -130,16 +143,16 @@ public class TheaterAppGUI {
 		gbc_nowShowingLabel.gridy = 3;
 		frame.getContentPane().add(nowShowingLabel, gbc_nowShowingLabel); 
 		
-		JButton movie1Button = new JButton("1");
-		movie1Button.setBackground(Color.WHITE);
-		movie1Button.setForeground(Color.WHITE);
-		ImageIcon movie1Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie1_Joker.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
-		movie1Button.setIcon(movie1Icon);
-		movie1Button.setBorder(BorderFactory.createEmptyBorder());
-		movie1Button.addActionListener(new MovieListener());
+//		JButton movie1Button = new JButton("1");
+//		movie1Button.setBackground(Color.WHITE);
+//		movie1Button.setForeground(Color.WHITE);
+//		ImageIcon movie1Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie1_Joker.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
+//		movie1Button.setIcon(movie1Icon);
+//		movie1Button.setBorder(BorderFactory.createEmptyBorder());
+//		movie1Button.addActionListener(new MovieListener());
 		
-		String comboBoxStrings[] = {CARD1, CARD2, CARD3};		//This is combo box for sorting methods, not functional right now
-		JComboBox comboBox = new JComboBox(comboBoxStrings);
+		String comboBoxStrings[] = {CARD1, CARD2, CARD3, CARD4, CARD5,CARD6,CARD7,CARD8};		//This is combo box for sorting methods, not functional right now
+		comboBox = new JComboBox(comboBoxStrings);
 		comboBox.setBackground(Color.WHITE);
 		comboBox.setFont(new Font("Helvetica", Font.ITALIC + Font.BOLD, 15));
 		comboBox.setEditable(false);
@@ -149,113 +162,215 @@ public class TheaterAppGUI {
 		gbc_comboBox.gridx = 2;
 		gbc_comboBox.gridy = 3;
 		frame.getContentPane().add(comboBox, gbc_comboBox);
+		
+		JButton btnSort = new JButton("Sort"); //Sorting button that can be clicked when a sorting option is chosen
+		btnSort.setFont(new Font("Dialog", Font.BOLD, 15));
+		GridBagConstraints gbc_btnSort = new GridBagConstraints();
+		gbc_btnSort.insets = new Insets(0, 0, 5, 5);
+		gbc_btnSort.gridx = 3;
+		gbc_btnSort.gridy = 3;
+		btnSort.addActionListener(new SortListener());
+		frame.getContentPane().add(btnSort, gbc_btnSort);
+		
+		JLabel lblMovies = new JLabel("Movies");
+		lblMovies.setFont(new Font("Dialog", Font.BOLD, 18));
+		GridBagConstraints gbc_lblMovies = new GridBagConstraints();
+		gbc_lblMovies.insets = new Insets(0, 0, 0, 5);
+		gbc_lblMovies.gridx = 1;
+		gbc_lblMovies.gridy = 6;
+		frame.getContentPane().add(lblMovies, gbc_lblMovies);
+		
+		comboBox_1 = new JComboBox();
+		comboBox_1.setFont(new Font("SansSerif", Font.BOLD, 15));
+		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
+		comboBox_1.setBackground(Color.WHITE);
+		gbc_comboBox_1.insets = new Insets(0, 0, 0, 5);
+		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_1.gridx = 2;
+		gbc_comboBox_1.gridy = 6;
+		frame.getContentPane().add(comboBox_1, gbc_comboBox_1);
+		
+		JButton btnChoose = new JButton("Choose");
+		btnChoose.setFont(new Font("Dialog", Font.BOLD, 15));
+		GridBagConstraints gbc_btnChoose = new GridBagConstraints();
+		gbc_btnChoose.insets = new Insets(0, 0, 0, 5);
+		gbc_btnChoose.gridx = 3;
+		gbc_btnChoose.gridy = 6;
+		frame.getContentPane().add(btnChoose, gbc_btnChoose);
 		GridBagConstraints gbc_movie1Button = new GridBagConstraints();
 		gbc_movie1Button.insets = new Insets(20, 120, 20, 20);
 		gbc_movie1Button.gridx = 1;
 		gbc_movie1Button.gridy = 6;
-		frame.getContentPane().add(movie1Button, gbc_movie1Button);
+//		frame.getContentPane().add(movie1Button, gbc_movie1Button);
+//		JButton movie2Button = new JButton("2");
+//		movie2Button.setBackground(Color.WHITE);
+//		movie2Button.setForeground(Color.WHITE);
+//		ImageIcon movie2Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie2_Midsommar.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
+//		movie2Button.setIcon(movie2Icon);
+//		movie2Button.setBorder(BorderFactory.createEmptyBorder());
+//		movie2Button.addActionListener(new MovieListener());
+//		GridBagConstraints gbc_movie2Button = new GridBagConstraints();
+//		gbc_movie2Button.insets = new Insets(20, 20, 20, 20);
+//		gbc_movie2Button.gridx = 2;
+//		gbc_movie2Button.gridy = 6;
+//		frame.getContentPane().add(movie2Button, gbc_movie2Button);
+//		
+//		JButton movie3Button = new JButton("3");
+//		movie3Button.setBackground(Color.WHITE);
+//		movie3Button.setForeground(Color.WHITE);
+//		ImageIcon movie3Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie3_OnceUpon.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
+//		movie3Button.setIcon(movie3Icon);
+//		movie3Button.setBorder(BorderFactory.createEmptyBorder());
+//		movie3Button.addActionListener(new MovieListener());
+//		GridBagConstraints gbc_movie3Button = new GridBagConstraints();
+//		gbc_movie3Button.insets = new Insets(20, 20, 20, 20);
+//		gbc_movie3Button.gridx = 3;
+//		gbc_movie3Button.gridy = 6;
+//		frame.getContentPane().add(movie3Button, gbc_movie3Button);
+//		
+//		JButton movie4Button = new JButton("4");
+//		movie4Button.setBackground(Color.WHITE);
+//		movie4Button.setForeground(Color.WHITE);
+//		ImageIcon movie4Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie4_LionKing.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
+//		movie4Button.setIcon(movie4Icon);
+//		movie4Button.setBorder(BorderFactory.createEmptyBorder());
+//		movie4Button.addActionListener(new MovieListener());
+//		GridBagConstraints gbc_movie4Button = new GridBagConstraints();
+//		gbc_movie4Button.insets = new Insets(20, 20, 20, 20);
+//		gbc_movie4Button.gridx = 4;
+//		gbc_movie4Button.gridy = 6;
+//		frame.getContentPane().add(movie4Button, gbc_movie4Button);
+//		
+//		JButton movie5Button = new JButton("5");
+//		movie5Button.setBackground(Color.WHITE);
+//		movie5Button.setForeground(Color.WHITE);
+//		ImageIcon movie5Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie5_Avengers.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
+//		movie5Button.setIcon(movie5Icon);
+//		movie5Button.setBorder(BorderFactory.createEmptyBorder());
+//		movie5Button.addActionListener(new MovieListener());
+//		GridBagConstraints gbc_movie5Button = new GridBagConstraints();
+//		gbc_movie5Button.insets = new Insets(0, 0, 0, 120);
+//		gbc_movie5Button.gridx = 5;
+//		gbc_movie5Button.gridy = 6;
+//		frame.getContentPane().add(movie5Button, gbc_movie5Button);
 		
-		JButton movie2Button = new JButton("2");
-		movie2Button.setBackground(Color.WHITE);
-		movie2Button.setForeground(Color.WHITE);
-		ImageIcon movie2Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie2_Midsommar.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
-		movie2Button.setIcon(movie2Icon);
-		movie2Button.setBorder(BorderFactory.createEmptyBorder());
-		movie2Button.addActionListener(new MovieListener());
-		GridBagConstraints gbc_movie2Button = new GridBagConstraints();
-		gbc_movie2Button.insets = new Insets(20, 20, 20, 20);
-		gbc_movie2Button.gridx = 2;
-		gbc_movie2Button.gridy = 6;
-		frame.getContentPane().add(movie2Button, gbc_movie2Button);
-		
-		JButton movie3Button = new JButton("3");
-		movie3Button.setBackground(Color.WHITE);
-		movie3Button.setForeground(Color.WHITE);
-		ImageIcon movie3Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie3_OnceUpon.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
-		movie3Button.setIcon(movie3Icon);
-		movie3Button.setBorder(BorderFactory.createEmptyBorder());
-		movie3Button.addActionListener(new MovieListener());
-		GridBagConstraints gbc_movie3Button = new GridBagConstraints();
-		gbc_movie3Button.insets = new Insets(20, 20, 20, 20);
-		gbc_movie3Button.gridx = 3;
-		gbc_movie3Button.gridy = 6;
-		frame.getContentPane().add(movie3Button, gbc_movie3Button);
-		
-		JButton movie4Button = new JButton("4");
-		movie4Button.setBackground(Color.WHITE);
-		movie4Button.setForeground(Color.WHITE);
-		ImageIcon movie4Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie4_LionKing.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
-		movie4Button.setIcon(movie4Icon);
-		movie4Button.setBorder(BorderFactory.createEmptyBorder());
-		movie4Button.addActionListener(new MovieListener());
-		GridBagConstraints gbc_movie4Button = new GridBagConstraints();
-		gbc_movie4Button.insets = new Insets(20, 20, 20, 20);
-		gbc_movie4Button.gridx = 4;
-		gbc_movie4Button.gridy = 6;
-		frame.getContentPane().add(movie4Button, gbc_movie4Button);
-		
-		JButton movie5Button = new JButton("5");
-		movie5Button.setBackground(Color.WHITE);
-		movie5Button.setForeground(Color.WHITE);
-		ImageIcon movie5Icon = new ImageIcon(new ImageIcon(getClass().getResource("Movie5_Avengers.jpg")).getImage().getScaledInstance(264, 396, Image.SCALE_SMOOTH));
-		movie5Button.setIcon(movie5Icon);
-		movie5Button.setBorder(BorderFactory.createEmptyBorder());
-		movie5Button.addActionListener(new MovieListener());
-		GridBagConstraints gbc_movie5Button = new GridBagConstraints();
-		gbc_movie5Button.insets = new Insets(0, 0, 0, 120);
-		gbc_movie5Button.gridx = 5;
-		gbc_movie5Button.gridy = 6;
-		frame.getContentPane().add(movie5Button, gbc_movie5Button);
-		
-		JButton checkoutButton = new JButton("Checkout");
-		checkoutButton.setBackground(Color.WHITE);
-		checkoutButton.setForeground(Color.BLACK);
-		checkoutButton.setFont(new Font("HelveticaNeue", Font.BOLD, 15));
-		checkoutButton.addActionListener(new CheckoutListener());
-		GridBagConstraints gbc_checkoutButton = new GridBagConstraints();
-		gbc_checkoutButton.insets = new Insets(5, 20, 10, 5);
-		gbc_checkoutButton.gridx = 5;
-		gbc_checkoutButton.gridy = 8;
-		frame.getContentPane().add(checkoutButton, gbc_checkoutButton);
+//		JButton checkoutButton = new JButton("Checkout");
+//		checkoutButton.setBackground(Color.WHITE);
+//		checkoutButton.setForeground(Color.BLACK);
+//		checkoutButton.setFont(new Font("HelveticaNeue", Font.BOLD, 15));
+//		checkoutButton.addActionListener(new CheckoutListener());
+//		GridBagConstraints gbc_checkoutButton = new GridBagConstraints();
+//		gbc_checkoutButton.insets = new Insets(5, 20, 10, 5);
+//		gbc_checkoutButton.gridx = 5;
+//		gbc_checkoutButton.gridy = 8;
+//		frame.getContentPane().add(checkoutButton, gbc_checkoutButton);
 
 	}
-	
+
 	private class SearchListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e) 
 		{
+			//Will be worked on soon
 			String movieTitleSearch = searchTextField.getText();
-			//Search movies based of user input
+
 		}
 	}
 	
-	private class MovieListener implements ActionListener //Send object of Movie as parameter so we can build the correct movie window
+	private class SortListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e) 
 		{
-			if(e.getActionCommand() == "1") {
-				MovieGUI newMovieWindow = new MovieGUI(movies[0]);
-				newMovieWindow.NewScreen(movies[0]);
+		
+			sortOption=(String)comboBox.getSelectedItem();
+			comboBox_1.removeAllItems(); //clears the jcombobox when the sort button is clicked
+			if(sortOption.equals(CARD1)) {
+				Collection<Movie> sortCollection = movie1.sortByTitle(movieCollection);	
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
 			}
-			else if(e.getActionCommand() == "2") {
-				MovieGUI newMovieWindow = new MovieGUI(movies[1]);
-				newMovieWindow.NewScreen(movies[1]);
+			else if(sortOption.equals(CARD2)){
+				Collection<Movie> sortCollection = movie1.filterGenres(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
 			}
-			else if(e.getActionCommand() == "3") {
-				MovieGUI newMovieWindow = new MovieGUI(movies[2]);
-				newMovieWindow.NewScreen(movies[2]);
+			else if(sortOption.equals(CARD3)){
+				Collection<Movie> sortCollection = movie1.filterGenres(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
 			}
-			else if(e.getActionCommand() == "4") {
-				MovieGUI newMovieWindow = new MovieGUI(movies[3]);
-				newMovieWindow.NewScreen(movies[3]);
+			else if(sortOption.equals(CARD4)){
+				Collection<Movie> sortCollection = movie1.filterGenres(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
 			}
-			else if(e.getActionCommand() == "5") {
-				MovieGUI newMovieWindow = new MovieGUI(movies[4]);
-				newMovieWindow.NewScreen(movies[4]);
+			else if(sortOption.equals(CARD5)){
+				Collection<Movie> sortCollection = movie1.filterGenres(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
 			}
+			else if(sortOption.equals(CARD6)){
+				Collection<Movie> sortCollection = movie1.filterRatings(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
+			}
+			else if(sortOption.equals(CARD7)){
+				Collection<Movie> sortCollection = movie1.filterRatings(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
+			}
+			else if(sortOption.equals(CARD8)){
+				Collection<Movie> sortCollection = movie1.filterRatings(movieCollection, sortOption);
+				for (Movie s : sortCollection) {
+				    comboBox_1.addItem(s.getTitle());
+				}	
+				
+			}
+
+
 		}
 	}
 	
+//	private class MovieListener implements ActionListener //Send object of Movie as parameter so we can build the correct movie window
+//	{
+//		public void actionPerformed(ActionEvent e) 
+//		{
+//			if(e.getActionCommand() == "1") {
+//				MovieGUI newMovieWindow = new MovieGUI(movies[0]);
+//				newMovieWindow.NewScreen(movies[0]);
+//			}
+//			else if(e.getActionCommand() == "2") {
+//				MovieGUI newMovieWindow = new MovieGUI(movies[1]);
+//				newMovieWindow.NewScreen(movies[1]);
+//			}
+//			else if(e.getActionCommand() == "3") {
+//				MovieGUI newMovieWindow = new MovieGUI(movies[2]);
+//				newMovieWindow.NewScreen(movies[2]);
+//			}
+//			else if(e.getActionCommand() == "4") {
+//				MovieGUI newMovieWindow = new MovieGUI(movies[3]);
+//				newMovieWindow.NewScreen(movies[3]);
+//			}
+//			else if(e.getActionCommand() == "5") {
+//				MovieGUI newMovieWindow = new MovieGUI(movies[4]);
+//				newMovieWindow.NewScreen(movies[4]);
+//			}
+//		}
+//	}
+//	
 	private class CheckoutListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e) 
@@ -264,4 +379,6 @@ public class TheaterAppGUI {
 			newCheckoutWindow.NewScreen();
 		}
 	}
+	
+
 }
