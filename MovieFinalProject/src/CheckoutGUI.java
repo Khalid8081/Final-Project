80e7a33d37485bf17b4a63491915b19f97a5a6bc
lanaberge <lanaberge@lanas-air.hsd1.mn.comcast.net>
2019-11-26 21:04:38 -0600

    Added removeTextField and removeButton, RemoveListener() not yet implemented but GUI is ready to go.

--- MovieFinalProject/src/CheckoutGUI.java
+++ MovieFinalProject/src/CheckoutGUI.java
@@ -3,7 +3,6 @@ import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
@@ -11,11 +10,14 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import java.awt.Insets;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
 import java.util.Iterator;
-import javax.swing.JPanel;
 
 
 public class CheckoutGUI {
@@ -27,7 +29,7 @@ public class CheckoutGUI {
 	public JTextArea subtotalTextArea;
 	public JTextArea ticketTextArea;
 	public static String ticketString=""; //Will be used for the ticket textArea //Look at TheaterAppGUI's checkout listener for more
-	
+	private JTextField removeTextField;
 
 	public static void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
@@ -44,11 +46,9 @@ public class CheckoutGUI {
 
 	public CheckoutGUI() {
 		initialize();
-		
 	}
 
 	private void initialize() {
-		
 		frame = new JFrame();
 		frame.setTitle("Checkout");
 		frame.setBounds(100, 100, 1447, 774);
@@ -75,24 +75,47 @@ public class CheckoutGUI {
 		gbc_ticketTextArea.fill = GridBagConstraints.BOTH;
 		gbc_ticketTextArea.gridx = 0;
 		gbc_ticketTextArea.gridy = 1;
+		gbc_ticketTextArea.gridwidth = 11;
+		gbc_ticketTextArea.fill = GridBagConstraints.HORIZONTAL;
 		ticketTextArea.setEditable(false);
 		frame.getContentPane().add(ticketTextArea, gbc_ticketTextArea);
 		
-		JLabel ticketLabel = new JLabel("");
-		GridBagConstraints gbc_ticketLabel = new GridBagConstraints();
-		gbc_ticketLabel.anchor = GridBagConstraints.EAST;
-		gbc_ticketLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_ticketLabel.gridx = 0;
-		gbc_ticketLabel.gridy = 2;
-		frame.getContentPane().add(ticketLabel, gbc_ticketLabel);
+//		JLabel ticketLabel = new JLabel("");							//Not sure what this widget is so I commented it out for now
+//		GridBagConstraints gbc_ticketLabel = new GridBagConstraints();
+//		gbc_ticketLabel.anchor = GridBagConstraints.EAST;
+//		gbc_ticketLabel.insets = new Insets(0, 0, 5, 5);
+//		gbc_ticketLabel.gridx = 0;
+//		gbc_ticketLabel.gridy = 2;
+//		frame.getContentPane().add(ticketLabel, gbc_ticketLabel);
 		
-		JPanel ticketPanel = new JPanel();
-		GridBagConstraints gbc_ticketPanel = new GridBagConstraints();
-		gbc_ticketPanel.insets = new Insets(0, 0, 5, 5);
-		gbc_ticketPanel.fill = GridBagConstraints.BOTH;
-		gbc_ticketPanel.gridx = 1;
-		gbc_ticketPanel.gridy = 2;
-		frame.getContentPane().add(ticketPanel, gbc_ticketPanel);
+//		JPanel ticketPanel = new JPanel();
+//		GridBagConstraints gbc_ticketPanel = new GridBagConstraints();		
+//		gbc_ticketPanel.insets = new Insets(0, 0, 5, 5);
+//		gbc_ticketPanel.fill = GridBagConstraints.BOTH;
+//		gbc_ticketPanel.gridx = 1;
+//		gbc_ticketPanel.gridy = 2;
+//		frame.getContentPane().add(ticketPanel, gbc_ticketPanel);
+		
+		removeTextField = new JTextField();
+		GridBagConstraints gbc_removeTextField = new GridBagConstraints();
+		gbc_removeTextField.anchor = GridBagConstraints.EAST;
+		gbc_removeTextField.insets = new Insets(0, 0, 5, 5);
+		gbc_removeTextField.gridx = 2;
+		gbc_removeTextField.gridy = 6;
+		frame.getContentPane().add(removeTextField, gbc_removeTextField);
+		removeTextField.setColumns(20);
+
+		JButton removeButton = new JButton("Remove ticket");
+		removeButton.setBackground(Color.WHITE);
+		removeButton.setFont(new Font("HelveticaNeue", Font.BOLD, 18));
+		removeButton.setForeground(Color.BLACK);
+		GridBagConstraints gbc_removeButton = new GridBagConstraints();
+		gbc_removeButton.anchor = GridBagConstraints.WEST;
+		gbc_removeButton.insets = new Insets(0, 0, 5, 5);
+		gbc_removeButton.gridx = 3;
+		gbc_removeButton.gridy = 6;
+		removeButton.addActionListener(new RemoveListener()); 	//RemoveListener() not get implemented
+		frame.getContentPane().add(removeButton, gbc_removeButton);
 		
 		JLabel subtotalLabel = new JLabel("Subtotal: ");
 		subtotalLabel.setBackground(Color.WHITE);
@@ -162,6 +185,16 @@ public class CheckoutGUI {
 		ticketTextArea.setText(ticketString);
 	}
 	
+	private class RemoveListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			//Needs to be implemented. Something we talked about in class was maybe indexing the user's tickets.
+			String ticketToBeRemoved = removeTextField.getText(); //Does not have to be String
+			
+		}
+	}
+	
 	private class PayListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e) 
@@ -188,8 +221,8 @@ public class CheckoutGUI {
 					subtotal=subtotal+iterator.next().getPrice();
 				}
 				
-				taxes=subtotal*TAX_RATE;
-				total=subtotal+taxes;
+				taxes = subtotal*TAX_RATE;
+				total =  subtotal+taxes;
 				//The total,subtotal and tax will need to be formatted to look nicer on the gui.
 				totalTextArea.setText(""+total);
 				subtotalTextArea.setText(""+subtotal);
