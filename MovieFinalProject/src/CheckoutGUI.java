763691dbb6e46bbc19fd0af48ef822773d2bb15d
lanaberge <lanaberge@lanas-air.hsd1.mn.comcast.net>
2019-11-27 19:53:28 -0600

    Removed frame, have initialize() return a JPanel. Adjusted GUI layout so it is ready for Remove feature to be implemented

--- MovieFinalProject/src/CheckoutGUI.java
+++ MovieFinalProject/src/CheckoutGUI.java
@@ -48,16 +48,9 @@ public class CheckoutGUI {
 		initialize();
 	}
 
-	private void initialize() {
-		frame = new JFrame();
-		frame.setTitle("Checkout");
-		frame.setBounds(100, 100, 1447, 774);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{173, 79, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 61, 61, 61, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
+	public JPanel initialize() {
+		
+		JPanel panel = new JPanel(new GridBagLayout());
 		
 		JLabel ticketsLabel = new JLabel("Your Tickets:");
 		ticketsLabel.setBackground(Color.WHITE);
@@ -67,7 +60,7 @@ public class CheckoutGUI {
 		gbc_ticketsLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_ticketsLabel.gridx = 0;
 		gbc_ticketsLabel.gridy = 0;
-		frame.getContentPane().add(ticketsLabel, gbc_ticketsLabel);
+		panel.add(ticketsLabel, gbc_ticketsLabel);
 		
 		ticketTextArea = new JTextArea();
 		GridBagConstraints gbc_ticketTextArea = new GridBagConstraints();
@@ -78,23 +71,7 @@ public class CheckoutGUI {
 		gbc_ticketTextArea.gridwidth = 11;
 		gbc_ticketTextArea.fill = GridBagConstraints.HORIZONTAL;
 		ticketTextArea.setEditable(false);
-		frame.getContentPane().add(ticketTextArea, gbc_ticketTextArea);
-		
-//		JLabel ticketLabel = new JLabel("");							//Not sure what this widget is so I commented it out for now
-//		GridBagConstraints gbc_ticketLabel = new GridBagConstraints();
-//		gbc_ticketLabel.anchor = GridBagConstraints.EAST;
-//		gbc_ticketLabel.insets = new Insets(0, 0, 5, 5);
-//		gbc_ticketLabel.gridx = 0;
-//		gbc_ticketLabel.gridy = 2;
-//		frame.getContentPane().add(ticketLabel, gbc_ticketLabel);
-		
-//		JPanel ticketPanel = new JPanel();
-//		GridBagConstraints gbc_ticketPanel = new GridBagConstraints();		
-//		gbc_ticketPanel.insets = new Insets(0, 0, 5, 5);
-//		gbc_ticketPanel.fill = GridBagConstraints.BOTH;
-//		gbc_ticketPanel.gridx = 1;
-//		gbc_ticketPanel.gridy = 2;
-//		frame.getContentPane().add(ticketPanel, gbc_ticketPanel);
+		panel.add(ticketTextArea, gbc_ticketTextArea);
 		
 		removeTextField = new JTextField();
 		GridBagConstraints gbc_removeTextField = new GridBagConstraints();
@@ -102,7 +79,7 @@ public class CheckoutGUI {
 		gbc_removeTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_removeTextField.gridx = 2;
 		gbc_removeTextField.gridy = 6;
-		frame.getContentPane().add(removeTextField, gbc_removeTextField);
+		panel.add(removeTextField, gbc_removeTextField);
 		removeTextField.setColumns(20);
 
 		JButton removeButton = new JButton("Remove ticket");
@@ -115,7 +92,7 @@ public class CheckoutGUI {
 		gbc_removeButton.gridx = 3;
 		gbc_removeButton.gridy = 6;
 		removeButton.addActionListener(new RemoveListener()); 	//RemoveListener() not get implemented
-		frame.getContentPane().add(removeButton, gbc_removeButton);
+		panel.add(removeButton, gbc_removeButton);
 		
 		JLabel subtotalLabel = new JLabel("Subtotal: ");
 		subtotalLabel.setBackground(Color.WHITE);
@@ -125,7 +102,7 @@ public class CheckoutGUI {
 		gbc_subtotalLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_subtotalLabel.gridx = 0;
 		gbc_subtotalLabel.gridy = 6;
-		frame.getContentPane().add(subtotalLabel, gbc_subtotalLabel);
+		panel.add(subtotalLabel, gbc_subtotalLabel);
 		
 		subtotalTextArea = new JTextArea();
 		GridBagConstraints gbc_subtotalTextArea = new GridBagConstraints();
@@ -133,7 +110,8 @@ public class CheckoutGUI {
 		gbc_subtotalTextArea.fill = GridBagConstraints.BOTH;
 		gbc_subtotalTextArea.gridx = 1;
 		gbc_subtotalTextArea.gridy = 6;
-		frame.getContentPane().add(subtotalTextArea, gbc_subtotalTextArea);
+		panel.add(subtotalTextArea, gbc_subtotalTextArea);
+		subtotalTextArea.setColumns(20);
 		
 		JLabel taxLabel = new JLabel("Tax: ");
 		taxLabel.setBackground(Color.WHITE);
@@ -143,7 +121,7 @@ public class CheckoutGUI {
 		gbc_taxLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_taxLabel.gridx = 0;
 		gbc_taxLabel.gridy = 7;
-		frame.getContentPane().add(taxLabel, gbc_taxLabel);
+		panel.add(taxLabel, gbc_taxLabel);
 		
 		taxTextArea = new JTextArea();
 		GridBagConstraints gbc_taxTextArea = new GridBagConstraints();
@@ -151,7 +129,8 @@ public class CheckoutGUI {
 		gbc_taxTextArea.fill = GridBagConstraints.BOTH;
 		gbc_taxTextArea.gridx = 1;
 		gbc_taxTextArea.gridy = 7;
-		frame.getContentPane().add(taxTextArea, gbc_taxTextArea);
+		panel.add(taxTextArea, gbc_taxTextArea);
+		taxTextArea.setColumns(20);
 		
 		JLabel totalLabel = new JLabel("Total: ");
 		totalLabel.setBackground(Color.WHITE);
@@ -161,7 +140,7 @@ public class CheckoutGUI {
 		gbc_totalLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_totalLabel.gridx = 0;
 		gbc_totalLabel.gridy = 8;
-		frame.getContentPane().add(totalLabel, gbc_totalLabel);
+		panel.add(totalLabel, gbc_totalLabel);
 		
 		totalTextArea = new JTextArea();
 		GridBagConstraints gbc_totalTextArea = new GridBagConstraints();
@@ -169,7 +148,8 @@ public class CheckoutGUI {
 		gbc_totalTextArea.fill = GridBagConstraints.BOTH;
 		gbc_totalTextArea.gridx = 1;
 		gbc_totalTextArea.gridy = 8;
-		frame.getContentPane().add(totalTextArea, gbc_totalTextArea);
+		panel.add(totalTextArea, gbc_totalTextArea);
+		totalTextArea.setColumns(20);
 		
 		JButton payButton = new JButton("Pay");
 		payButton.setBackground(Color.WHITE);
@@ -180,9 +160,11 @@ public class CheckoutGUI {
 		gbc_payButton.insets = new Insets(0, 0, 20, 20);
 		gbc_payButton.gridx = 10;
 		gbc_payButton.gridy = 9;
-		frame.getContentPane().add(payButton, gbc_payButton);
+		panel.add(payButton, gbc_payButton);
 		 
 		ticketTextArea.setText(ticketString);
+		
+		return panel;
 	}
 	
 	private class RemoveListener implements ActionListener
@@ -202,21 +184,21 @@ public class CheckoutGUI {
 			//To clear the text Areas
 			totalTextArea.setText("");
 			subtotalTextArea.setText("");
-			double total=0.0;
-			double subtotal=0.0;
-			double taxes=0.0;
+			double total = 0.0;
+			double subtotal = 0.0;
+			double taxes = 0.0;
 			
 			//ticketTextArea
-			if(MovieGUI.accountCreated==false) {
+			if(MovieGUI.accountCreated == false) {
 				SignInGUI newSignUpWindow = new SignInGUI();
 				newSignUpWindow.NewScreen();
-				MovieGUI.accountCreated=true;
+				MovieGUI.accountCreated = true;
 			}
 			else if(TheaterAppGUI.customer.getCustomerTickets().isEmpty()) {
-				JOptionPane.showMessageDialog(frame, "Cart is empty!");
+				JOptionPane.showMessageDialog(frame, "Your cart is empty!");
 			}
 			else {
-				Iterator<Ticket> iterator= (Iterator)TheaterAppGUI.customer.getCustomerTickets().iterator();
+				Iterator<Ticket> iterator= (Iterator) TheaterAppGUI.customer.getCustomerTickets().iterator();
 				while(iterator.hasNext()){
 					subtotal=subtotal+iterator.next().getPrice();
 				}
@@ -229,17 +211,13 @@ public class CheckoutGUI {
 				taxTextArea.setText(""+taxes);
 				
 				if(TheaterAppGUI.customer.getBalance()<total) {
-					JOptionPane.showMessageDialog(frame, "Balance is low!"+ ticketString);
-					
+					JOptionPane.showMessageDialog(frame, "Your balance is low!"+ ticketString);
 				}
 				else {
 					TheaterAppGUI.customer.setBalance(TheaterAppGUI.customer.getBalance()-total);
-					JOptionPane.showMessageDialog(frame, "Purchased successful!");
+					JOptionPane.showMessageDialog(frame, "Tickets purchased successful!");
 				}
-				
 			}
-
 		}
 	}
-
 }
